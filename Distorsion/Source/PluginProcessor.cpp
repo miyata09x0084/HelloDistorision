c1fa97577620bf9e1e221e3a248519e16866fdd5
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-01-13 16:07:21 +0900

    クリッピング処理にコントロール用パラメータを組み込む

--- Distorsion/Source/PluginProcessor.cpp
+++ Distorsion/Source/PluginProcessor.cpp
@@ -22,6 +22,10 @@ DistorsionAudioProcessor::DistorsionAudioProcessor()
                        )
 #endif
 {
+    UserParams[MasterBypass] = 0.0f;
+    UserParams[Gain] = 0.7f;
+    UserParams[Threshold] = 1.0f;
+    UserParams[Volume] = 0.7f;
 }
 
 DistorsionAudioProcessor::~DistorsionAudioProcessor()
@@ -133,38 +137,31 @@ void DistorsionAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, j
     auto totalNumInputChannels  = getTotalNumInputChannels();
     auto totalNumOutputChannels = getTotalNumOutputChannels();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
+    
+    if (UserParams[MasterBypass] == 1.0f) return;
+    
+    buffer.applyGain(pow(UserParams[Gain], 2) * 2.0f);
+    float threshold = pow(UserParams[Threshold], 2);
+    
     for (int channel = 0; channel < totalNumInputChannels; ++channel)
     {
         float* channelData = buffer.getWritePointer (channel);
 
         for (long buffNum = 0; buffNum<buffer.getNumSamples(); buffNum++)
         {
-            float thresold =    0.7f;
-            if (channelData[buffNum] >= thresold)
+            if (channelData[buffNum] >= threshold)
             {
-                channelData[buffNum] = thresold;
+                channelData[buffNum] = threshold;
             }
-            else if (channelData[buffNum] <= -thresold)
+            else if (channelData[buffNum] <= -threshold)
             {
-                channelData[buffNum] = -thresold;
+                channelData[buffNum] = -threshold;
             }
         }
     }
+    buffer.applyGain(pow(UserParams[Volume], 2) * 2.0f);
 }
 
 //==============================================================================
